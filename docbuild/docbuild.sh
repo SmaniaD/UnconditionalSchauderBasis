8b084d06d2baa09cc9f41e2b3b23aea1644e74ff
Daniel Smania <daniel.smania@gmail.com>
2026-05-10 19:50:55 -0300

    docs

--- docbuild/docbuild.sh
+++ docbuild/docbuild.sh
@@ -2,7 +2,7 @@ MATHLIB_NO_CACHE_ON_UPDATE=1 lake update doc-gen
 lake build UnconditionalSchauderBasis:docs
 rm -rf ../docs
 mkdir ../docs
-cp .lake/build/doc/. ../docs/
+cp -r .lake/build/doc/. ../docs/
 cd ../docs/
 python3 -m http.server
 
